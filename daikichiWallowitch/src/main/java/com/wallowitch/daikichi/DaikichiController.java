681fedb90826dbedcd00fa7c6de939b45227bec6
Joshua Wallowitch <55968853+JPWallow@users.noreply.github.com>
2022-08-22 15:10:31 -0400

    Add files via upload
    Added path variables.

--- daikichiWallowitch/src/main/java/com/wallowitch/daikichi/DaikichiController.java
+++ daikichiWallowitch/src/main/java/com/wallowitch/daikichi/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.wallowitch.daikichi;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +19,16 @@ public class DaikichiController {
 	public String tomorrow( ) {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
 	}
-	
+	@RequestMapping("/travel/{city}")
+	public String travel(@PathVariable("city") String city) {
+		return "Congratulations! You will soon travel to " + city + "!";
+	}
+	@RequestMapping("/lotto/{number}")
+	public String lotto(@PathVariable("number") Integer number) {
+		if (number%2==0) { 
+			return "You will take a grand journey in the near future, but be weary of tempting offers.";}
+		else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}				
+	}
 }
