b389eb0451274c33b1d398f6e74a7829a55b0054
Joshua Wallowitch <55968853+JPWallow@users.noreply.github.com>
2022-08-22 14:47:33 -0400

    Add files via upload

---
+++ daikichiWallowitch/src/main/java/com/wallowitch/daikichi/DaikichiWallowitchApplication.java
@@ -0,0 +1,13 @@
+package com.wallowitch.daikichi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichiWallowitchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiWallowitchApplication.class, args);
+	}
+
+}
