41afabf0398318802c3b8ea32c31f4492a24fae8
Joshua Wallowitch <55968853+JPWallow@users.noreply.github.com>
2022-08-22 20:40:33 -0400

    Add files via upload

---
+++ fruityLoopsWallowitch/src/main/java/com/wallowitch/fruityLoopsWallowitch/FruityLoopsWallowitchApplication.java
@@ -0,0 +1,13 @@
+package com.wallowitch.fruityLoopsWallowitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruityLoopsWallowitchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruityLoopsWallowitchApplication.class, args);
+	}
+
+}
---
+++ fruityLoopsWallowitch/src/test/java/com/wallowitch/fruityLoopsWallowitch/FruityLoopsWallowitchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallowitch.fruityLoopsWallowitch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruityLoopsWallowitchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
